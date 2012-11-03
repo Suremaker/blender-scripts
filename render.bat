b7b5daf90b5c35a2cadf9ea4f5b3d9b29be6ce41
Wojtek <wojciech.kotlarski@gmail.com>
2012-11-03 12:26:28 +0000

    added tile2x1 and box2x1

--- render.bat
+++ render.bat
@@ -1,1 +1,2 @@
-blender -b tile.blend -P tile-render.py -- file=long_grass.blend zooming=4 out=.\
+blender -b tile2x1.blend -P tile-render.py -- file=box2x1.blend zooming=4 out=.\
+rem blender -b tile.blend -P tile-render.py -- file=long_grass.blend zooming=4 out=.\
